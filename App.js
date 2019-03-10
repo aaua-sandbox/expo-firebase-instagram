4ff23ac7a77f889a171d95ac9984b3d9cd61bbd4
aaua <15936438+aaua@users.noreply.github.com>
2019-03-10 13:14:28 +0900

    add eslint, load image and font

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  AppLoading,
+  Asset,
+  Font
+} from 'expo';
+import fonts from 'app/src/fonts';
+import images from 'app/src/images';
 
 export default class App extends React.Component {
+  static defaultProps = {
+    skipLoadingScreen: false,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoadingComplete: false,
+    };
+  }
+
+  loadResourcesAsync = async () => {
+    // ローカルフォルダーから画像をロード
+    await Asset.loadAsync(Object.keys(images).map(key => images[key]));
+    // ローカルフォルダーからフォントのロード
+    await Font.loadAsync(fonts);
+
+    return true;
+  };
+
   render() {
+    const { isLoadingComplete } = this.state;
+    const { skipLoadingScreen } = this.props;
+
+    // リソースのロードが終わるまでAppLoadingをrender
+    if (!isLoadingComplete && !skipLoadingScreen) {
+      return (
+        <AppLoading
+          // 非同期でリソースをロード
+          startAsync={this.loadResourcesAsync}
+          onError={error => console.warn(error)}
+          // リソースのロードが終わったらロードを終了
+          onFinish={() => this.setState({
+            isLoadingComplete: true
+          })}
+        />
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>Hello World</Text>
       </View>
     );
   }
@@ -14,8 +60,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontFamily: 'noto-sans-bold',
+  }
 });
